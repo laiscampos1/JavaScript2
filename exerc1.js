1aa2afbd6a820a1ce794cf54f5680f070be48f62
Laís Campos <131169073+laiscampos1@users.noreply.github.com>
2024-09-11 13:08:28 -0300

    Update exerc1.js

--- exerc1.js
+++ exerc1.js
@@ -1,3 +1,5 @@
+//Exercício realizado por Lais Campos e Pamella Sotomayor
+
 function exerc1() {
             let obj = {numeros: [1,3,7,9,-1,0,4], qt: 7};
             obj.numeros.sort();
@@ -11,10 +13,13 @@ function exerc1() {
                 somaTotal = somaTotal + obj.numeros[i];
                 container.appendChild(linha);
             }
-
+            let qntDiv = document.createElement('div');
+            qntDiv.innerHTML = "Quantidade: " + obj.qt;
             let somaDiv = document.createElement('div');
             somaDiv.innerHTML = "Soma Total: " + somaTotal;
-
+            container.appendChild(qntDiv);
             container.appendChild(somaDiv);
             document.body.appendChild(container);
     }
+
+//Laís e Pamella
